3c324ea2309f117e8c01136bffa3ff9a0cf79d56
Kirill Kobylyanskiy <kirill.kobylyanskiy@emlid.com>
2018-10-11 12:07:36 +0300

    neutis: initramfs: run e2fsck instead of fsck
    - get rid of get_fstype function which is not used
    - get rid of unused fix parameter

--- meta-neutis-bsp/recipes-bsp/neutis-initramfs/files/fsck.sh
+++ meta-neutis-bsp/recipes-bsp/neutis-initramfs/files/fsck.sh
@@ -2,18 +2,15 @@
 # $1=device
 checkfs_once() {
   DEV="$1"
-
-  TYPE=$(get_fstype "$1")
-
   FSCKCODE=0
- 
-  if ! command -v fsck >/dev/null 2>&1; then
-    log_warning_msg "fsck not present, so skipping $DEV"
-    return
-  fi
-	
+
   if [ "$fastboot" = "y" ]; then
     log_warning_msg "Fast boot enabled, so skipping $DEV check."
+    return
+  fi
+
+  if ! command -v e2fsck >/dev/null 2>&1; then
+    log_warning_msg "e2fsck not present, so skipping $DEV"
     return
   fi
 
@@ -23,30 +20,8 @@ checkfs_once() {
     force=""
   fi
 
-  if [ "$fsckfix" = "y" ]; then
-    fix="-y"
-  elif [ "$fsckfix" = "n" ]; then
-    fix="-n"
-  else
-    fix="-a"
-  fi
-
-  spinner=""
-  if [ -z "${debug}" ]; then
-    spinner="-C"
-  fi
-
-  if [ "${quiet}" = n ]; then
-    log_begin_msg "Will now check $DEV file system"
-    fsck $force $fix -V -t $TYPE $DEV
-    FSCKCODE=$?
-    log_end_msg
-  else
-    log_begin_msg "Checking $DEV file system"
-    fsck $force $fix -T -t $TYPE $DEV
-    FSCKCODE=$?
-    log_end_msg
-  fi
+  e2fsck -y $force $DEV
+  FSCKCODE=$?
 
   # NOTE: "failure" is defined as exiting with a return code of
   # 4, possibly or-ed with other flags. A return code of 1
@@ -68,9 +43,6 @@ checkfs_once() {
 }
 
 checkfs() {
-  ln -s /sbin/fsck.ext4 /bin/fsck.ext4
-  ln -s /sbin/fsck.ext2 /bin/fsck.ext2
-  ln -s /sbin/fsck.fat /bin/fsck.fat
   local DEV="$1"
 
   if [[ -b $DEV ]]; then
@@ -78,26 +50,4 @@ checkfs() {
 	  panic "The $2 filesystem on $1 requires a manual fsck"
     done
   fi
-}
-
-# Parameter: device node to check
-# Echos fstype to stdout
-# Return value: indicates if an fs could be recognized
-get_fstype () {
-  local FS FSTYPE FSSIZE RET
-  FS="${1}"
-
-  if command -v blkid >/dev/null 2>&1; then
-    FSTYPE=$(blkid -o value -s TYPE "${FS}")
-  elif [ -x /lib/udev/vol_id ]; then
-    FSTYPE=$(/lib/udev/vol_id -t "${FS}" 2> /dev/null)
-  fi
-  RET=$?
-
-  if [ -z "${FSTYPE}" ]; then
-    FSTYPE="unknown"
-  fi
-
-  echo "${FSTYPE}"
-  return ${RET}
 }
